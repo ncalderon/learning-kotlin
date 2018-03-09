386c2bcc60b495da25bfa43774a0f4e318e86d75
Nathaniel Calderon <nathaniel.calderon@gmail.com>
2018-03-09 16:31:23 -0400

    sending working test

--- src/main/kotlin/com/calderon/learning/app.kt
+++ src/main/kotlin/com/calderon/learning/app.kt
@@ -1,6 +1,14 @@
 package com.calderon.learning
 
 fun main(args: Array<String>) : Unit {
-    var name = "Nathaniel"
-    println("Hello, ${name}!")
+    var name: String?
+    name = null
+    print(max(name?.toInt(), name?.toInt()))
+
+}
+
+fun max(a: Int?, b: Int?) = if(a == null || b == null) a else if (a > b) a else b
+
+fun test(name: String) : Unit {
+    print("Arg: ${name}")
 }
---
+++ src/main/kotlin/com/calderon/learning/gretter.kt
@@ -0,0 +1,7 @@
+package com.calderon.learning
+
+class Gretter(val name: String){
+    fun greet(){
+        print("Hello, ${name}")
+    }
+}
